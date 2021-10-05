9877511587f9116318ed5e5880aff7d5cb3377dd
Ben-you <b.ikeh@yahoo.com>
2021-10-05 16:05:45 +0000

    add image field

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -9,14 +9,14 @@ const routes: Routes = [
     component: RecipesPage,
     pathMatch: 'full'
   },
-  // {
-  //   path: ':recipeId',
-  //   loadChildren: () => import('./recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule),
-  // },
   {
     path: 'addRecipe',
     loadChildren: () => import('./add-recipe/add-recipe.module').then( m => m.AddRecipePageModule)
-  }
+  },
+  {
+    path: ':recipeId',
+    loadChildren: () => import('./recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule),
+  },
 ];
 
 @NgModule({
