1d5f2d78c9c3b00a547e63586a238a11a0f78684
Benjamin Ikeh <b.ikeh@yahoo.com>
2022-01-14 16:59:47 +0000

    rebuild android app

--- src/app/recipes/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-item/recipe-item.component.ts
@@ -1,5 +1,7 @@
 import { Recipe } from './../recipe.model';
 import { Component, OnInit, Input } from '@angular/core';
+import { RecipesService } from '../recipes.service';
+import { ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,9 +9,24 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./recipe-item.component.scss'],
 })
 export class RecipeItemComponent implements OnInit {
-@Input() recipeItem: Recipe;
-  constructor() { }
+  @Input() recipeItem: Recipe;
 
-  ngOnInit() {}
+  loadedRecipe: Recipe;
+  constructor(private recipeService: RecipesService, private activatedRoute: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  deleteRecipeItem(){
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('recipeId')){
+        this.router.navigate(['/recipes']);
+        return;
+      }
+      const recipeId = paramMap.get('recipeId');
+      this.loadedRecipe = this.recipeService.getRecipe(recipeId);
+    });
+    return this.recipeService.deleteRecipe(this.loadedRecipe.id);
+  }
 
 }
